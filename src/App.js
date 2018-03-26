8425079754849fbbe675f485fc7ff1d84d6b6447
igor.sebelev <igor.sebelev@chulakov.ru>
2018-03-26 17:54:14 +0300

    Remove temp files

--- src/App.js
+++
@@ -1,6 +0,0 @@
-export class App {
-	constructor(a, b) {
-		this.a = a;
-		this.b = b;
-	}
-}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { App } from './App.js';
-let testPrettier = () => {
-	return '1+1';
-};
-let app = new App();
