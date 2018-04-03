373ee72e7d549561daa1faca8f4b968f9c498646
igor.sebelev <igor.sebelev@chulakov.ru>
2018-04-03 20:06:42 +0300

    New TODOs

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 //TODO: add package.json engines field
 //TODO: add message about successfully created component
 //TODO: use this.path instead of process.cwd() by replacing all cosmiconfig.config paths to absolute
+//TODO: handle error if "templates" array is empty
 
 const config = cosmiconfig('templateman', {
 	sync: true,
