b8426f9df640c0bb6b3e8ec31933826911eac6d1
igor.sebelev <igor.sebelev@chulakov.ru>
2018-04-04 15:46:59 +0300

    Change node engine version, test app on node@4, edit TODOs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@
 import cosmiconfig from 'cosmiconfig';
 import App from './App';
 
-//TODO: add package.json engines field
+//TODO: add package.json keywords
+//TODO: create Logger
 //TODO: add message about successfully created component
-//TODO: use this.path instead of process.cwd() by replacing all cosmiconfig.config paths to absolute
-//TODO: handle error if "templates" array is empty
 
 const config = cosmiconfig('templateman', {
 	sync: true,
