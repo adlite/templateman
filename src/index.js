5f22bc2592f70e8733b4c916f7303a4f4d977093
igor.sebelev <igor.sebelev@chulakov.ru>
2018-04-05 21:20:46 +0300

    Add package.json keywords, remove TODOs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@
 import cosmiconfig from 'cosmiconfig';
 import App from './App';
 
-//TODO: add package.json keywords
-//TODO: create Logger
-//TODO: add message about successfully created component
-
 const config = cosmiconfig('templateman', {
 	sync: true,
 	rcExtensions: true,
