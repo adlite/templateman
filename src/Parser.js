e74d6cc1cf6b3c88bf98ca077f2d620c88a55f85
Adlite <igor@chillicode.ru>
2021-10-24 03:10:02 +0300

    Add transformers feature

--- src/Parser.js
+++ src/Parser.js
@@ -1,22 +1,45 @@
 import uniq from 'lodash.uniq';
+import caser from 'caesar-caser';
 
 const Parser = {
-  regVarname: /\${TM:([A-Za-z0-9_]+)}/gm,
-  regVarnameOnce: /\${TM:([A-Za-z0-9_]+)}/,
+  regExpVarName: /\${TM:([A-Z0-9_]+):?([A-Z0-9_]+)?}/gm,
+  regExpVarNameOnce: /\${TM:([A-Z0-9_]+):?([A-Z0-9_]+)?}/,
+
+  transformers: {
+    CAMEL_CASE: val => caser(val).convertTo('camel'),
+    PASCAL_CASE: val => caser(val).convertTo('pascal'),
+    KEBAB_CASE: val => caser(val).convertTo('kebab'),
+    TRAIN_CASE: val => caser(val).convertTo('train'),
+    SNAKE_CASE: val => caser(val).convertTo('snake'),
+    CONSTANT_CASE: val => caser(val).convertTo('constant'),
+    DOT_CASE: val => caser(val).convertTo('dot'),
+    UPPER_DOT_CASE: val => caser(val).convertTo('upper-dot'),
+    SENTENCE_CASE: val => caser(val).convertTo('sentence'),
+    CAP_SENTENCE_CASE: val => caser(val).convertTo('cap-sentence'),
+    UPPER_SENTENCE_CASE: val => caser(val).convertTo('upper-sentence'),
+    LOWER_SENTENCE_CASE: val => caser(val).convertTo('lower-sentence'),
+  },
 
   getVars: data => {
     let res;
     let vars = [];
-    while ((res = Parser.regVarname.exec(data))) {
-      vars.push(res[1]);
+    while ((res = Parser.regExpVarName.exec(data))) {
+      const [match, varName] = res;
+      vars.push(varName);
     }
     return uniq(vars);
   },
 
   replaceVars: (data, values) => {
-    return data.replace(Parser.regVarname, foundStr => {
-      const varName = foundStr.match(Parser.regVarnameOnce)[1];
-      return values[varName];
+    return data.replace(Parser.regExpVarName, foundStr => {
+      const [match, varName, transformerName] = foundStr.match(Parser.regExpVarNameOnce);
+      const transformer = Parser.transformers[transformerName];
+
+      if (typeof transformer === 'function') {
+        return transformer(values[varName]);
+      } else {
+        return values[varName];
+      }
     });
   },
 };
